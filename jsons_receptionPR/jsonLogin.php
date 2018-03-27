c9120d2b49b653629f99c079f4ba99d73e7da802
Maxilei <maxime.iori@gmail.com>
2018-03-27 16:15:26 +0200

    MaJ file login php

--- jsons_receptionPR/jsonLogin.php
+++ jsons_receptionPR/jsonLogin.php
@@ -2,11 +2,11 @@
 include("mini.php");
 if(isset($_POST['login'])&&isset($_POST['password']))
 {
-	$req="select identifiant, nom, prenom from utilisateur where login='".$_POST['login']."' and motDePasse=password('".$_POST['password']."')";
+	$req="SELECT userMail, userNom, userPrenom FROM utilisateur WHERE userMail='".$_POST['login']."' and userMdp='".$_POST['password']."' and userRole='PtRl'";
 	$res=mysqli_query($base,$req);
 	$tabInfo=mysqli_fetch_array($res);
 	if(mysqli_num_rows($res)==1)
-	$_SESSION['identifiant']=$tabInfo[0];
+	$_SESSION['login']=$tabInfo[0];
 	echo json_encode($tabInfo);
 }
 else
