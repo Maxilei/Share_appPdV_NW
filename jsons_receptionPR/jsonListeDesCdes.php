3953df2c0f75afae341920f9771a08abd59ec063
Maxilei <maxime.iori@gmail.com>
2018-03-27 17:18:34 +0200

    correction script PHP liste produit par producteur du jour

--- jsons_receptionPR/jsonListeDesCdes.php
+++ jsons_receptionPR/jsonListeDesCdes.php
@@ -1,29 +1,31 @@
 <?php 
 include("mini.php");
-if(isset($_SESSION['login']))
-    $pointRelaisMail = $_SESSION['login'];
-	if(isset($_POST['utilisateurID'])){
-		$prodId = $_POST['utilisateurID'];
-	}
+if(isset($_SESSION['login']) && isset($_POST['utilisateurID']))
 {
+    $pointRelaisMail = $_SESSION['login'];
+	$producteurId = $_POST['utilisateurID'];
 	$resultat=array();
     $req="
-    	SELECT lotDescription 
-    	FROM pointRelais  
-    		INNER JOIN LDC  
-    			ON pointRelais.prID=LDC.prID 
-    		INNER JOIN commande 
-    			ON LDC.cmdID=commande.cmdID 
-    		INNER JOIN producteur 
-    			ON LDC.producID=producteur.producID 
-    		INNER JOIN utilisateur 
-    			ON producteur.utilisateurID = utilisateur.utilisateurID 
-    		INNER JOIN lot 
-    			ON LDC.lotID = lot.lotID;
-    	WHERE userRole = 'Prod'
-			AND cmdDateLivraison=CURDATE()
-			AND pointRelais.prID=(SELECT  prID FROM pointRelais INNER JOIN utilisateur ON pointRelais.utilisateurID = utilisateur.utilisateurID WHERE userMail='user7@gmail.com');
-			AND utilisateurID=".$prodId.";";
+	    	SELECT  lotDescription, LDC.qte, uniteMesure.umNom
+			FROM pointRelais  
+				INNER JOIN LDC  
+					ON pointRelais.prID=LDC.prID
+				INNER JOIN producteur 
+					ON LDC.producID=producteur.producID 
+				INNER JOIN commande 
+					ON LDC.cmdID=commande.cmdID 
+				INNER JOIN consommateur 
+					ON commande.consID=consommateur.consID 
+				INNER JOIN utilisateur 
+					ON consommateur.utilisateurID = utilisateur.utilisateurID 
+				INNER JOIN lot 
+					ON LDC.lotID = lot.lotID
+				INNER JOIN uniteMesure 
+					ON lot.umId=uniteMesure.umId
+			WHERE userRole = 'Cons'
+				AND cmdDateLivraison=CURDATE()
+				AND pointRelais.prID=(SELECT  prID FROM pointRelais INNER JOIN utilisateur ON pointRelais.utilisateurID = utilisateur.utilisateurID WHERE userMail='user7@gmail.com')
+				AND producteur.producID = (SELECT producteur.producID FROM producteur INNER JOIN utilisateur ON producteur.utilisateurID = utilisateur.utilisateurID WHERE utilisateur.utilisateurID= ".$producteurId." )";
 	
 	$res=mysqli_query($base,$req);
 	while($tabInfos=mysqli_fetch_assoc($res))
@@ -34,26 +36,5 @@ if(isset($_SESSION['login']))
 }
 else
 print_r($_SESSION);
-
-"
-SELECT  lotDescription, cmdDateLivraison, LDC.qte, uniteMesure.umNom
-FROM pointRelais  
-INNER JOIN LDC  
-ON pointRelais.prID=LDC.prID 
-INNER JOIN commande 
-ON LDC.cmdID=commande.cmdID 
-INNER JOIN producteur 
-ON LDC.producID=producteur.producID 
-INNER JOIN uniteMesure 
-ON lot.umId=uniteMesure.umID
-INNER JOIN utilisateur 
-ON producteur.utilisateurID = utilisateur.utilisateurID 
-INNER JOIN lot 
-ON LDC.lotID = lot.lotID
-WHERE userRole = 'Prod'
-AND cmdDateLivraison=CURDATE()
-AND pointRelais.prID=(SELECT  prID FROM pointRelais INNER JOIN utilisateur ON pointRelais.utilisateurID = utilisateur.utilisateurID WHERE userMail='user7@gmail.com');
-AND utilisateurID=".$prodId.";";
-"
 
 ?>
--- jsons_receptionPR/prodDuJour.php
+++ jsons_receptionPR/prodDuJour.php
@@ -1,8 +1,8 @@
 <?php
 include("mini.php");
 if(isset($_SESSION['login']))
-    $pointRelaisMail =$_SESSION['login'];
 {   
+    $pointRelaisMail =$_SESSION['login'];
     //Requete qui ressort les producteurs qui auront une livraison demain
 	$resultat=array();
     $req="
@@ -18,7 +18,7 @@ if(isset($_SESSION['login']))
                 ON producteur.utilisateurID = utilisateur.utilisateurID
         WHERE userRole = 'Prod'
             AND cmdDateLivraison=CURDATE()
-            AND pointRelais.prID=(SELECT  prID FROM pointRelais INNER JOIN utilisateur ON pointRelais.utilisateurID = utilisateur.utilisateurID WHERE userMail='".$pointRelaisMail."');"
+            AND pointRelais.prID=(SELECT  prID FROM pointRelais INNER JOIN utilisateur ON pointRelais.utilisateurID = utilisateur.utilisateurID WHERE userMail='".$pointRelaisMail."');";
 //AND cmdDateLivraison=(SELECT DATE_ADD(curdate(), INTERVAL 1 DAY) AS Tomorrow)
 
 	$res=mysqli_query($base,$req);
