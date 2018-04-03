9f6df81e546b43e5f39f7cf39fd7cbc88490d52d
Maxilei <maxime.iori@gmail.com>
2018-04-03 16:51:44 +0200

    Requetes utiles.

--- jsons_receptionPR/prodDuJour.php
+++ jsons_receptionPR/prodDuJour.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['login']))
     //Requete qui ressort les producteurs qui auront une livraison demain
 	$resultat=array();
     $req="
-        SELECT userNom, userPrenom 
+        SELECT userNom, userPrenom, utilisateur.utilisateurID
         FROM pointRelais
             INNER JOIN LDC
                 ON pointRelais.prID=LDC.prID
@@ -17,7 +17,7 @@ if(isset($_SESSION['login']))
             INNER JOIN utilisateur 
                 ON producteur.utilisateurID = utilisateur.utilisateurID
         WHERE userRole = 'Prod'
-            AND cmdDateLivraison=CURDATE()
+            AND cmdDateLivraison='2018-03-27'
             AND pointRelais.prID=(SELECT  prID FROM pointRelais INNER JOIN utilisateur ON pointRelais.utilisateurID = utilisateur.utilisateurID WHERE userMail='".$pointRelaisMail."');";
 //AND cmdDateLivraison=(SELECT DATE_ADD(curdate(), INTERVAL 1 DAY) AS Tomorrow)
 
