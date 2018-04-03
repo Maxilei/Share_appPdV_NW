11d7487d7ec2725eb2d6eb99c71d9855c1852815
Maxilei <maxime.iori@gmail.com>
2018-04-03 17:57:08 +0200

    Autres fichiers requetes

---
+++ jsons_receptionPR/nbCommandeDuCons.php
@@ -0,0 +1,35 @@
+<?php
+include("mini.php");
+$_SESSION['login']=$_GET['login'];
+$_POST['utilisateurID']=$_GET['userId'];
+if(isset($_SESSION['login']) && isset($_POST['utilisateurID']))
+{   
+    $consommateurId = $_POST['utilisateurID'];
+    $pointRelaisMail =$_SESSION['login'];
+    //Requete qui ressort les producteurs qui auront une livraison demain
+	$resultat=array();
+    $req="
+        SELECT COUNT(commande.cmdID) as nbCommande
+        FROM pointRelais
+            INNER JOIN commande
+                ON pointRelais.prID=commande.prID
+            INNER JOIN consommateur 
+                ON commande.consID=consommateur.consID
+            INNER JOIN utilisateur 
+                ON consommateur.utilisateurID = utilisateur.utilisateurID
+        WHERE userRole = 'Cons'
+            AND utilisateur.utilisateurID = '".$consommateurId."'
+            AND cmdDateLivraison='2018-03-27'
+            AND pointRelais.prID=(SELECT  prID FROM pointRelais INNER JOIN utilisateur ON pointRelais.utilisateurID = utilisateur.utilisateurID WHERE userMail='".$pointRelaisMail."')
+        GROUP BY utilisateur.utilisateurID";
+        echo $req;
+	$res=mysqli_query($base,$req);
+	while($tabInfos=mysqli_fetch_assoc($res))
+	{
+		$resultat[]=$tabInfos;
+	}
+	echo json_encode($resultat);
+}
+else
+print_r($_SESSION);
+?>
